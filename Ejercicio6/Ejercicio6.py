690fc4282024e90fb4de1ace17f8360d7132dd8b
Juan <105739344+contra3000@users.noreply.github.com>
2022-05-23 12:28:34 -0300

    PyEx6 Openbootcamp
    Se sube ejercicio incluyendo capturas de pantallas de la consola, como así también el código en Python.

---
+++ Ejercicio6/Ejercicio6.py
@@ -0,0 +1,7 @@
+
+peso = float(input("Introduzca su peso en kg: "))
+altura = float(input("Introduzca su altura en metros: "))
+
+IMC = peso/(altura**2)
+
+print(f"Su indice de masa corporal es: {IMC:.2f}")
